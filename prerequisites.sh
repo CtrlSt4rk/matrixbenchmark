92c200d24d1a0a0a8c14d34532ca034a80cccd25
Andre <56407521+CtrlSt4rk@users.noreply.github.com>
2024-08-22 11:53:16 +0100

    added support for different terminal packages

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install neofetch
-sudo apt install clang
+clear
+echo "Choose an option for a terminal install of the following packages:"
+echo "- neofetch"
+echo "- clang"
+echo ""
+echo "[0] apt"
+echo "[1] dnf"
+echo "[2] brew"
+echo ""
+echo "Press any other key to quit"
+
+read -s -n 1 keybd
+case $keybd in
+		0) apt;;
+		1) dnf;;
+		2) brew;;
+	esac
+
+function apt(){
+    sudo apt update
+    sudo apt install neofetch
+    sudo apt install clang
+}
+
+function dnf(){
+    sudo dnf update
+    sudo dnf install neofetch
+    sudo dnf install clang
+}
+
+function brew(){
+    brew update
+    brew install neofetch
+    #clang already preinstalled (Sonoma+)
+}
